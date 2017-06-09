9b007bc7d2406dc9c348d1f83a4575a758a99bb0
Adrian Rowbotham <aderowbotham@gmail.com>
2017-06-09 11:57:28 +0100

    updated to 1.0.0

--- dist/ng-shadowbox.js
+++ dist/ng-shadowbox.js
@@ -1,48 +1,52 @@
-'use strict';
+(function(){
 
-angular.module('ngShadowbox', []).directive('ngShadowbox', function($window) {
+  "use strict";
 
-  return {
-    restrict: 'A',
-    replace: false,
-    link: function(scope, element, attrs){
+  angular.module("ngShadowbox", []).directive("ngShadowbox", function($window) {
 
-      if(!$window.Shadowbox){
-        throw Error("Shadowbox is not loaded");
+    return {
+      restrict: "A",
+      replace: false,
+      link: function(scope, element, attrs){
+
+        if(!$window.Shadowbox){
+          throw Error("Shadowbox is not loaded");
+        }
+
+        $window.Shadowbox.init({
+          skipSetup: true
+        });
+
+        if(!attrs.ngShadowbox){
+          throw Error("No Shadobox target found");
+        }
+
+        var playerTitle = attrs.title ? attrs.title : "Shadowbox";
+        var playerType = attrs.ngShadowboxPlayer ? attrs.ngShadowboxPlayer : "img";
+        var gallery = attrs.ngShadowboxGallery ? attrs.ngShadowboxGallery : "ng-shadowbox";
+        var width = attrs.ngShadowboxWidth ? attrs.ngShadowboxWidth : false;
+
+        element.bind("click", function() {
+
+          var opts = {
+            content:    attrs.ngShadowbox,
+            title:      playerTitle,
+            player:     playerType,
+            gallery:    gallery,
+            options:  {
+              handleOversize: "resize"
+            }
+          };
+
+          if(width){
+            opts.width = width;
+          }
+
+          $window.Shadowbox.open(opts);
+        });
       }
+    };
 
-      $window.Shadowbox.init({
-        skipSetup: true
-      });
+  });
 
-      if(!attrs.ngShadowbox){
-        throw Error("No Shadobox target found"); 
-      } 
-
-      var playerTitle = attrs.title ? attrs.title : "Shadowbox";
-      var playerType = attrs.ngShadowboxPlayer ? attrs.ngShadowboxPlayer : "img";
-      var gallery = attrs.ngShadowboxGallery ? attrs.ngShadowboxGallery : "ng-shadowbox";
-      var width = attrs.ngShadowboxWidth ? attrs.ngShadowboxWidth : false;
-
-      element.bind('click', function() {
-
-        var opts = {
-          content:    attrs.ngShadowbox,
-          title:      playerTitle,
-          player:     playerType,
-          gallery:    gallery,
-          options:  {
-            handleOversize: "resize"
-          }       
-        }
-
-        if(width){
-          opts.width = width;
-        }
-
-        $window.Shadowbox.open(opts);
-      });
-    }
-  }
-
-});
+}());
